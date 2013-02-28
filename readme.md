66d89b4cc2108811d04cc002d5b23158e335bab8
Joe Tiseo <jtiseo@gmail.com>
2013-02-27 23:04:31 -0500

    cleaned up code

--- readme.md
+++ readme.md
@@ -3,5 +3,10 @@
 Downloaded the latest uploaded youtube videos from your favorite channels.
 
 
-
+###Requried###
 Python 2.7.3
+[youtube-dl](https://github.com/rg3/youtube-dl/)
+
+###To Do###
+* Add error for invalid youtube user
+* Allow for more than 25 videos to be downloaded
--- youtube_download.py
+++ youtube_download.py
@@ -17,7 +17,7 @@ while True:
         author = raw_input("Enter username of YouTube channel: ")
         break
     except ValueError:
-        print "Please enter a valid number"
+        print "Please enter a valid username"
 while True:
     try:
         number_of_videos = int(raw_input("Enter number of videos to download (1-25): "))
@@ -33,18 +33,19 @@ inp.close()
 x=0
 y=len(resp)
 while x < number_of_videos: #25:#1:
-	first = resp['feed']['entry'][x]#[0]
-	#print first
+	video = resp['feed']['entry'][x]#[0]
+	
 	# Title of the video
-	title = first['title']['$t']
+	title = video['title']['$t']
 	print title
 
-	# URL
-	link = first['link'][0]['href']
+	# URL of the video
+	link = video['link'][0]['href']
 	print link
-	x = x + 1
-
+	
 	d = date.today().strftime("%Y-%m-%d")
 
-	call(["youtube-dl", "-c", link, "-o", 'downloads/%(title)s-%(upload_date)s'])
-	call(["rm", "-f", "*.part"]) # remove all the partial downlaods
+	call(["youtube-dl", "-c", link, "-o", 'downloads/%(title)s-%(upload_date)s']) # send url to youtube-dl
+	call(["rm", "-f", "*.part"]) # remove all partial download files
+
+	x = x + 1
